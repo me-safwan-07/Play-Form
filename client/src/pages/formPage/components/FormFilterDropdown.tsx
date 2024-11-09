aac5cb538aa569ef50cf42d9bc659818f101cdd9
Muhammed Safwan <167682973+me-safwan-07@users.noreply.github.com>
2024-11-09 09:34:49 +0530

    add the FormDropdownMenu file

--- client/src/pages/formPage/components/FormFilterDropdown.tsx
+++ client/src/pages/formPage/components/FormFilterDropdown.tsx
@@ -1,14 +1,25 @@
-// interface FormFilterDropdownProps {
-//     title: string;
-// }
+import { DropdownMenu } from "@/components/ui/DropdownMenu"
+import { DropdownMenuContent, DropdownMenuTrigger } from "@radix-ui/react-dropdown-menu"
+import { ChevronDownIcon } from "lucide-react"
 
 // export const FormFilterDropdown = ({title}: FormFilterDropdownProps) => {
-export const FormFilterDropdown = () => {
+
+export const FormFilterDropdown = (
+
+) => {
 
   return (
-    <div>
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <div className="">
+          <span></span>
+          <ChevronDownIcon />
+        </div>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
         
-    </div>
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
 
