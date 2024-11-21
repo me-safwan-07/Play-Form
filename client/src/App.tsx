219c94aa2c8f8fa01aad5422feeb10cae1c1b0f7
Muhammed Safwan <167682973+me-safwan-07@users.noreply.github.com>
2024-11-21 08:05:39 +0530

    createdstructure of welcome card

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { SigninForm } from './components/core/auth/login/components/SigninForm';
 import Page from './pages/formPage/page';
 import { TemplateContainer } from './pages/templates/components/TemplateContainer';
+import { Edit } from './pages/edit';
+import { EditWelcomeCard } from './pages/edit/components/EditWelcomeCard';
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Page />} />
         <Route path="/a" element={<TemplateContainer />} />
+        <Route path="/edit" element={<EditWelcomeCard activeQuestionId="start"/>} />
       </Routes>
     </Router>
   );
