947026ed90c73f84eb799b39fc7b3df9e66c14e1
Muhammed Safwan <167682973+me-safwan-07@users.noreply.github.com>
2024-12-08 21:36:39 +0530

    created Formeditor file

---
+++ client/src/pages/edit/components/FormEditor.tsx
@@ -0,0 +1,31 @@
+import { PreviewForm } from "@/components/ui/PreviewSurvey"
+import { FormMenuBar } from "./FormMenuBar"
+import { QuestionsAudienceTabs } from "./QuestionsStylingSettingsTabs"
+import { QuestionsView } from "./QuestionsView"
+
+export const FormEditor =() => {
+    return (
+        <>
+            <div className="flex h-full w-full flex-col">
+                <FormMenuBar />
+                <div className="relative z-0 flex flex-1 overflow-hidden">
+                    <main
+                        className="relative z-0 w-1/2 flex-1 overflow-y-auto focus:outline-none"
+                    >
+                        <QuestionsAudienceTabs  
+                            activeId="questions"
+                            isStylingTabVisible={true}
+                        />
+
+                        <QuestionsView />
+                    </main>
+                    <aside className="group hidden flex-1 flex-shrink-0 items-center justify-center overflow-hidden border-l border  border-slate-100 bg-slate-50 py-6 md:flex md:flex-col">
+                        <PreviewForm 
+                            previewType= "modal"
+                        />
+                    </aside>
+                </div>
+            </div>
+        </>
+    )
+}
