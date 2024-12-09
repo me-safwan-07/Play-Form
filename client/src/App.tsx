1d8381932ae30ef0b1f3f46fb26d2e920adbf121
me_safwan_07 <mesafwan07@gmail.com>
2024-12-09 23:51:02 +0530

    add not found routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { SigninForm } from './components/core/auth/login/components/SigninForm';
 import Page from './pages/formPage/page';
 import { TemplateContainer } from './pages/templates/components/TemplateContainer';
 import { Edit } from './pages/edit';
-import { EditWelcomeCard } from './pages/edit/components/EditWelcomeCard';
 import { AddQuestionButton } from './pages/edit/components/AddQuestionButton';
 import { QuestionCard } from './pages/edit/components/QuestionCard';
 import { SelectQuestionChoice } from './pages/edit/components/SelectQuestionChoice';
 import { FormMenuBar } from './pages/edit/components/FormMenuBar';
 import { FormEditor } from './pages/edit/components/FormEditor';
+import Auth from './pages/auth';
+import NotFound from './components/ui/NotFound';
 
 function App() {
   return (
@@ -23,6 +23,8 @@ function App() {
         <Route path="/test1" element={<SelectQuestionChoice />} />
         <Route path="/menu" element={<FormMenuBar />} />
         <Route path="/editor" element={<FormEditor />} />
+        <Route path="/auth/*" element={<Auth />} />
+        <Route path="/not-found" element={<NotFound />} />
       </Routes>
     </Router>
   );
