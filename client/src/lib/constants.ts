957478709c7ac4ce39483ef08281ddcd018fede3
me_safwan_07 <mesafwan07@gmail.com>
2024-12-11 21:35:46 +0530

    add some constants

---
+++ client/src/lib/constants.ts
@@ -0,0 +1,4 @@
+export const GOOGLE_OAUTH_ENABLED =
+  import.meta.env.VITE_GOOGLE_CLIENT_ID && import.meta.env.VITE_GOOGLE_CLIENT_SECRET ? true : false;
+export const EMAIL_AUTH_ENABLED = import.meta.env.VITE_EMAIL_AUTH_DISABLED !== "1";
+export const PASSWORD_RESET_DISABLED = import.meta.env.VITE_PASSWORD_RESET_DISABLED === "1";
