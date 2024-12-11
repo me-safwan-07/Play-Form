075eb0efbfb0e8c7e8ce762e3cc4aef1cf7e538c
me_safwan_07 <mesafwan07@gmail.com>
2024-12-11 21:49:11 +0530

    ForgotPassword page

---
+++ client/src/pages/auth/forgot-password/index.tsx
@@ -0,0 +1,12 @@
+import { FormWrapper } from "../components/FormWrapper"
+import { PasswordResetFrom } from "./components/PasswordResetFrom";
+
+const ForgotPassword = () => {
+    return (
+        <FormWrapper>
+            <PasswordResetFrom />
+        </FormWrapper>
+    );
+};
+
+export default ForgotPassword;
