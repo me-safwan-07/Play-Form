bb47da6b3f39025ec010320babb180b487a1e5c9
me_safwan_07 <mesafwan07@gmail.com>
2024-12-11 21:50:20 +0530

    add SignupForm comp

---
+++ client/src/pages/auth/signup/components/SignupForm.tsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import { SignupOptions } from "@/components/ui/SignupOptions";
+import { XCircleIcon } from "lucide-react"
+
+interface SignupFormProps {
+    emailAuthEnabled: boolean;
+    passwordResetEnabled: boolean;
+    googleOAuthEnabled: boolean;
+}
+
+export const SignupForm = ({
+    emailAuthEnabled,
+    passwordResetEnabled,
+    googleOAuthEnabled
+}: SignupFormProps) => {
+    const [error, setError] = useState<string>("");
+    return (
+        <>
+            {error && (
+                <div className="absolute top-10 rounded-md bg-red-50 p-4">
+                <div className="flex">
+                    <div className="flex-shrink-0">
+                    <XCircleIcon className="h-5 w-5 text-red-400" aria-hidden="true" />
+                    </div>
+                    <div className="ml-3">
+                    <h3 className="text-sm font-medium text-red-800">An error occurred when signing you up</h3>
+                    <div className="mt-2 text-sm text-red-700">
+                        <p className="space-y-1 whitespace-pre-wrap">{error}</p>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            )}
+
+            <div className="">
+                <h1 className="mb-4 text-slate-700">Create your Formbricks account</h1>
+                <SignupOptions 
+                    emailAuthEnabled={emailAuthEnabled}
+                    passwordResetEnabled={passwordResetEnabled}
+                    googleOAuthEnabled={googleOAuthEnabled}
+                />
+            </div>
+        </>
+    )
+}
