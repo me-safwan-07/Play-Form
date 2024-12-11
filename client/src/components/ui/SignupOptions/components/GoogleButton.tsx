ca3f3eadc37e6e0b1b8d2cfc793febf3dbc29b0f
me_safwan_07 <mesafwan07@gmail.com>
2024-12-11 21:30:49 +0530

    create googleButton

---
+++ client/src/components/ui/SignupOptions/components/GoogleButton.tsx
@@ -0,0 +1,26 @@
+import Button from "../../Button/index";
+import { GoogleIcon } from "../../icons/GoogleIcon";
+
+export const GoogleButton = ({
+  text = "Continue with Google",
+  handleGoogleSignIn,
+  inviteUrl,
+}: {
+  text?: string;
+  handleGoogleSignIn: () => void;
+  inviteUrl?: string | null;
+}) => {
+
+  return (
+    <Button
+      type="button"
+      EndIcon={GoogleIcon}
+      startIconClassName="ml-3"
+      onClick={handleGoogleSignIn}
+      variant="secondary"
+      className="w-full justify-center"
+    >
+      {text}
+    </Button>
+  );
+};
