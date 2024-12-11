9f8d62f49c50fdf945c0bee990bbb424f3739c7b
me_safwan_07 <mesafwan07@gmail.com>
2024-12-11 22:04:38 +0530

    updated router folder

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,10 +1,13 @@
 import express, { Express, Request, Response } from "express";
 import personRoutes from './personRoutes';
 import displayRoutes from './displayRoutes';
+import userRoutes from './userRoutes';
 
 const app: Express = express();
 
 app.use("/person", personRoutes);
 app.use("/display", displayRoutes);
+app.use("/form", personRoutes);
+app.use("/user", userRoutes);
 
 export default app;
