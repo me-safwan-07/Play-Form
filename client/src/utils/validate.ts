7193905018f0c064f3832f9c8ef1f3c9530106fd
me_safwan_07 <mesafwan07@gmail.com>
2024-12-11 21:46:46 +0530

    add validateInputs

---
+++ client/src/utils/validate.ts
@@ -0,0 +1,17 @@
+import z from "zod";
+import { ValidationError } from "../types/errors";
+
+type ValidationPair = [any, z.ZodSchema<any>]
+
+export const validateInputs = (...pairs: ValidationPair[]): void => {
+  for (const [value, schema] of pairs) {
+    const inputValidation = schema.safeParse(value);
+
+    if (!inputValidation.success) {
+      console.error(
+        `Validation failed for ${JSON.stringify(value).substring(0, 100)} and ${JSON.stringify(schema)}: ${inputValidation.error.message}`
+      );
+      throw new ValidationError("Validation failed");
+    }
+  }
+};
