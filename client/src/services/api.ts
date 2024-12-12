8e11662331fbf29aa5b0e738a6fdfef3e7b87bdf
me_safwan_07 <mesafwan07@gmail.com>
2024-12-12 09:01:58 +0530

    add userendpoints

---
+++ client/src/services/api.ts
@@ -0,0 +1,7 @@
+export const userEndPoints = {
+    GET_USER_BY_ID: 'http://localhost:3000/api/user', // { id }
+    getUser: '/api/users/:id',
+    createUser: '/api/users',
+    updateUser: '/api/users/:id',
+    deleteUser: '/api/users/:id',
+}
