6f2ca9d8fc93bdc33c678ebc310f3bfcad0ed562
me_safwan_07 <mesafwan07@gmail.com>
2024-12-12 09:03:56 +0530

    add user controller

--- server/dist/controllers/userController/index.js
+++ server/dist/controllers/userController/index.js
@@ -9,8 +9,19 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.createUserController = void 0;
+exports.deleteUser = exports.updateUserById = exports.getUserById = exports.createUserController = void 0;
 const userService_1 = require("../../services/userService");
+const database_1 = require("../../database");
+const responseSelection = {
+    id: true,
+    name: true,
+    email: true,
+    emailVerified: true,
+    imageUrl: true,
+    createdAt: true,
+    updatedAt: true,
+    identityProvider: true,
+};
 const createUserController = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const userData = req.body;
@@ -25,3 +36,59 @@ const createUserController = (req, res, next) => __awaiter(void 0, void 0, void 
     }
 });
 exports.createUserController = createUserController;
+const getUserById = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const { id } = req.params;
+        const user = yield database_1.prisma.user.findUnique({
+            where: { id },
+            select: responseSelection
+        });
+        if (!user) {
+            res.status(404).json({ success: false, message: 'User not found' });
+            return;
+        }
+        ;
+        res.json(user);
+        next();
+    }
+    catch (error) {
+        res.status(404).json({ error: 'Failed to get user' });
+        next(error);
+    }
+});
+exports.getUserById = getUserById;
+const updateUserById = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id } = req.params;
+    try {
+        const updatedUser = yield database_1.prisma.user.update({
+            where: { id },
+            data: req.body,
+            select: responseSelection
+        });
+        if (!updatedUser) {
+            res.status(404).json({ success: false, message: 'User not found' });
+            return;
+        }
+        res.json(updatedUser);
+    }
+    catch (err) {
+        res.status(500).json({ error: 'Failed to update user' });
+        next(err);
+    }
+});
+exports.updateUserById = updateUserById;
+const deleteUser = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const { id } = req.params;
+    try {
+        const deletedUser = yield database_1.prisma.user.delete({
+            where: { id }
+        });
+        res.status(204).send('User deleted successfully');
+        next();
+    }
+    catch (error) {
+        res.status(500).json({ error: 'Failed to delete user' });
+        next(error);
+    }
+});
+exports.deleteUser = deleteUser;
--- server/src/controllers/userController/index.ts
+++ server/src/controllers/userController/index.ts
@@ -1,6 +1,18 @@
 import { Request, Response, NextFunction } from "express";
 import { CreateUser } from "../../services/userService";
 import { TUserCreateInput } from "../../types/user";
+import { prisma } from "../../database";
+
+const responseSelection = {
+  id: true,
+  name: true,
+  email: true,
+  emailVerified: true,
+  imageUrl: true,
+  createdAt: true,
+  updatedAt: true,
+  identityProvider: true,
+};
 
 export const createUserController = async (
   req: Request<{}, {}, TUserCreateInput>, 
@@ -20,3 +32,61 @@ export const createUserController = async (
     next(error);
   }
 };
+
+export const getUserById = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  try {
+    const { id } = req.params;
+    const user = await prisma.user.findUnique({
+      where: { id },
+      select: responseSelection
+    });
+
+    if (!user) {
+      res.status(404).json({ success: false, message: 'User not found' });
+      return 
+    };
+
+    res.json(user);
+    next();
+  } catch (error) {
+    res.status(404).json({ error: 'Failed to get user' });
+    next(error);
+  }
+};
+
+export const updateUserById = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+  const { id } = req.params;
+  try {
+    const updatedUser = await prisma.user.update({
+      where: { id },
+      data: req.body,
+      select: responseSelection
+    });
+
+    if (!updatedUser) {
+      res.status(404).json({ success: false, message: 'User not found' });
+      return 
+    }
+
+    res.json(updatedUser);
+  } catch (err) {
+    res.status(500).json({ error: 'Failed to update user' });
+    next(err);
+  }
+};
+
+export const deleteUser = async (req: Request, res: Response, next: NextFunction) => {
+  const { id } = req.params;
+  try {
+    const deletedUser = await prisma.user.delete({
+      where: { id }
+    });
+    res.status(204).send('User deleted successfully');
+    next();
+  } catch (error) {
+    res.status(500).json({ error: 'Failed to delete user' });
+    next(error);
+  }
+};
+
+
