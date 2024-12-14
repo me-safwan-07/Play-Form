489397936fc2c000ec071d7c8855402a7e44c15f
me_safwan_07 <mesafwan07@gmail.com>
2024-12-14 23:03:03 +0530

    add SURVEYS_PER_PAGE

--- client/src/lib/constants.ts
+++ client/src/lib/constants.ts
@@ -2,3 +2,6 @@ export const GOOGLE_OAUTH_ENABLED =
   import.meta.env.VITE_GOOGLE_CLIENT_ID && import.meta.env.VITE_GOOGLE_CLIENT_SECRET ? true : false;
 export const EMAIL_AUTH_ENABLED = import.meta.env.VITE_EMAIL_AUTH_DISABLED !== "1";
 export const PASSWORD_RESET_DISABLED = import.meta.env.VITE_PASSWORD_RESET_DISABLED === "1";
+
+
+export const SURVEYS_PER_PAGE = 12;
