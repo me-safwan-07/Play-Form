9aa2c1efd5de14fb6e65b63c16698302ece740da
Muhammed safwan <mesafwan07@gmail.com>
2024-12-25 21:36:11 +0530

    Enhance FileInput component: add props for allowed file extensions, form ID, and file URLs; implement state management for selected files and upload status

--- client/src/components/ui/FileInput/index.tsx
+++ client/src/components/ui/FileInput/index.tsx
@@ -1,4 +1,13 @@
+import { TAllowedFileExtension } from "@/types/common";
 import { Uploader } from "./components/Uploader";
+import { useState } from "react";
+
+interface FileInputProps {
+  allowedFileExtensions: TAllowedFileExtension[];
+  formId: string | undefined;
+  fileUrls: string[] | undefined;
+  htmlFor?: string
+}
 
 const allowedFileTypesForPreview = ["png", "jpeg", "jpg", "webp"];
 
@@ -7,16 +16,17 @@ const tabs = [
   { id: "video", label: "Video" },
 ];
 
-interface SelectedFile {
-  url: string;
-  name: string;
-  uploaded: Boolean;
-}
-
-export const FileInput = () => {
+export const FileInput = ({
+  allowedFileExtensions,
+  formId,
+  fileUrls,
+  htmlFor = "",
+}) => {
+  const [selectedFiles, setSelectedFiles] = useState<File[]>([]);
+  const [isUploading, setIsUploading] = useState(false);
 
     return (
-        <div className="w-full cursor-default">
+        <div className="fb-items-left fb-bg-input-bg hover:fb-bg-input-bg-selected fb-border-border fb-relative fb-mt-3 fb-flex fb-w-full fb-flex-col fb-justify-center fb-rounded-lg fb-border-2 fb-border-dashed dark:fb-border-slate-600 dark:fb-bg-slate-700 dark:hover:fb-border-slate-500 dark:hover:fb-bg-slate-800">
             <div className="flex flex-wrap gap-2">
                 <Uploader 
                      
