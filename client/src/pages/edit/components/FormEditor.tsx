7ebc4309ca990263cd43e1836cd37821fad0d914
me-safwan-07 <mesafwan07@gmail.com>
2025-01-11 23:45:54 +0530

    Refactor FormEditor component to integrate QuestionsAudienceTabs and manage active view state

--- client/src/pages/edit/components/FormEditor.tsx
+++ client/src/pages/edit/components/FormEditor.tsx
@@ -1,9 +1,9 @@
 import { PreviewForm } from "@/components/ui/PreviewSurvey"
 import { FormMenuBar } from "./FormMenuBar"
-// import { QuestionsAudienceTabs } from "./QuestionsStylingSettingsTabs"
+import { QuestionsAudienceTabs } from "./QuestionsStylingSettingsTabs"
 import { QuestionsView } from "./QuestionsView"
-import { TForm } from "@/types/forms";
-import { useEffect, useState } from "react";
+import { TForm, TFormEditorTabs } from "@/types/forms";
+import { useEffect, useRef, useState } from "react";
 import { structuredClone } from "@/lib/pollyfills/structuredClone";
 import { LoadingSkeleton } from "./LoadingSkeleton";
 
@@ -13,8 +13,10 @@ interface FormEditorProps {
 export const FormEditor =({
     form
 }: FormEditorProps) => {
+    const [activeView, setActiveView] = useState<TFormEditorTabs>("questions");
     const [activeQuestionId, setActiveQuestionId] = useState<string | null>(null);
     const [localForm, setLocalForm] = useState<TForm | null>(() => structuredClone(form));
+    const formEditorRef = useRef(null);
 
     useEffect(() => {
         if(form) {
@@ -43,25 +45,33 @@ export const FormEditor =({
     return (
         <>
             <div className="flex h-full w-full flex-col">
-                <FormMenuBar 
+                <FormMenuBar
+                    form={form}
                     localForm={localForm}
                     setLocalForm={setLocalForm}
+                    activeId={activeView}
+                    setActiveId={setActiveView}
                 />
                 <div className="relative z-0 flex flex-1 overflow-hidden">
                     <main
                         className="relative z-0 w-1/2 flex-1 overflow-y-auto focus:outline-none"
+                        ref={formEditorRef}
                     >
-                        {/* <QuestionsAudienceTabs 
-                            activeId="questions"
+                        <QuestionsAudienceTabs 
+                            activeId={activeView}
+                            setActiveId={setActiveView}
                             isStylingTabVisible={true}
-                        /> */}
-
-                        <QuestionsView 
-                            localForm={localForm}
-                            setLocalForm={setLocalForm}
-                            activeQuestionId={activeQuestionId}
-                            setActiveQuestionId={setActiveQuestionId}
                         />
+
+                        {activeView === "questions" && (
+                            <QuestionsView 
+                                localForm={localForm}
+                                setLocalForm={setLocalForm}
+                                activeQuestionId={activeQuestionId}
+                                setActiveQuestionId={setActiveQuestionId}
+                            />
+                        )}
+
                     </main>
                     <aside className="group hidden flex-1 flex-shrink-0 items-center justify-center overflow-hidden border-l border  border-slate-100 bg-slate-50 py-6 md:flex md:flex-col">
                         <PreviewForm 
