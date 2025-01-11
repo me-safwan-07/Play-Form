22ae11b43537ef8569fc41ebe7f1520652cf6211
me-safwan-07 <mesafwan07@gmail.com>
2025-01-11 23:46:41 +0530

    Enhance QuestionsView component with moveQuestion functionality and integrate QuestionsDroppable for improved question management

--- client/src/pages/edit/components/QuestionsStylingSettingsTabs.tsx
+++ client/src/pages/edit/components/QuestionsStylingSettingsTabs.tsx
@@ -44,25 +44,26 @@ export const QuestionsAudienceTabs = ({
         return tabs.filter((tab) => tab.id !== "styling");
       }, [isStylingTabVisible]);
     return (
-        <div className="fixed z-30 flex h-14 w-full items-center justify-center border bg-white md:w-1/2">
-            <nav className="flex h-full items-center space-x-4" aria-label="Tabs">
-                {tabsComputed.map((tab) => (
-                    <button
-                        type="button" 
-                        key={tab.id}
-                        onClick={() => setActiveId(tab.id)}
-                        className={cn(
-                            tab.id === activeId 
-                                ? "border-brand-dark border-b-2 font-semibold text-slate-900"
-                                : "text-slate-500 hover:text-slate-700",
-                            "text-slate-500 hoverflex h-full items-center px-3 text-sm font-medium"
-                        )}
-                    >
-                        {tab.icon && <div className="mr-2 h-5 w-5">{tab.icon}</div>} 
-                        {tab.label}   
-                    </button>
-                ))}
-            </nav>
-        </div>
+      <div className="fixed z-30 flex h-14 w-full items-center justify-center border bg-white md:w-1/2">
+        <nav className="flex h-full items-center space-x-4" aria-label="Tabs">
+          {tabsComputed.map((tab) => (
+              <button
+                  type="button" 
+                  key={tab.id}
+                  onClick={() => setActiveId(tab.id)}
+                  className={cn(
+                      tab.id === activeId 
+                          ? "border-brand-dark border-b-2 font-semibold text-slate-900"
+                          : "text-slate-500 hover:text-slate-700",
+                      "text-slate-500 hoverflex h-full items-center px-3 text-sm font-medium"
+                  )}
+                  aria-current={tab.id === activeId ? 'page' : undefined}
+              >
+                  {tab.icon && <div className="mr-2 h-5 w-5">{tab.icon}</div>} 
+                  {tab.label}   
+              </button>
+          ))}
+        </nav>
+      </div>
     )
 }
--- client/src/pages/edit/components/QuestionsView.tsx
+++ client/src/pages/edit/components/QuestionsView.tsx
@@ -1,8 +1,8 @@
 import React, { SetStateAction } from "react";
-// import { AddQuestionButton } from "./AddQuestionButton";
-// import { EditThankYouCard } from "./EditThankYouCard";
+import { AddQuestionButton } from "./AddQuestionButton";
+import { EditThankYouCard } from "./EditThankYouCard";
 import { EditWelcomeCard } from "./EditWelcomeCard";
-// import { QuestionsDroppable } from "./QuestionsDroppable";
+import { QuestionsDroppable } from "./QuestionsDroppable";
 import { TForm } from "@/types/forms";
 
 interface QuestionViewProps {
@@ -27,6 +27,15 @@ export const QuestionsView = ({
     //         updatedForm = handle
     //     }
     // }
+
+    const moveQuestion = (questionIndex: number, up: boolean) => {
+        const newQuestions = Array.from(localForm.questions);
+        const [reorderedQuestion] = newQuestions.splice(questionIndex, 1);
+        const destinationIndex = up ? questionIndex - 1 : questionIndex + 1;
+        newQuestions.splice(destinationIndex, 0, reorderedQuestion);
+        const updatedForm = { ...localForm, questions: newQuestions };
+        setLocalForm(updatedForm);
+    }
     return (
         <div className="mt-16 w-full px-5 py-4">
             <div className="mb-5 flex w-full flex-col gap-5">
@@ -38,12 +47,19 @@ export const QuestionsView = ({
                 />
             </div>
             
-            {/* <QuestionsDroppable /> */}
+            <QuestionsDroppable 
+                localForm={localForm}
+                moveQuestion={moveQuestion}
+                activeQuestionId={activeQuestionId}
+                setActiveQuestionId={setActiveQuestionId}
+            />
 
-            {/* <AddQuestionButton /> */}
-            {/* <div className="mt-5 flex flex-col gap-5">
-                <EditThankYouCard />
-            </div> */}
+            <AddQuestionButton />
+            <div className="mt-5 flex flex-col gap-5">
+                <EditThankYouCard 
+                    localForm={localForm}
+                />
+            </div>
         </div>
     )
 };
