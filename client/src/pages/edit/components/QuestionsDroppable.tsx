02dd0b1aa6bcb76668335774cbc0d98ea4797844
me-safwan-07 <mesafwan07@gmail.com>
2025-01-11 23:46:26 +0530

    Refactor QuestionsDroppable component to integrate SortableContext and enhance question management with activeQuestionId and moveQuestion props

--- client/src/pages/edit/components/QuestionsDroppable.tsx
+++ client/src/pages/edit/components/QuestionsDroppable.tsx
@@ -1,17 +1,35 @@
+import { SortableContext, verticalListSortingStrategy } from "@dnd-kit/sortable";
+
+
 import { TForm } from "@/types/forms"
 import { QuestionCard } from "./QuestionCard"
 
 interface QuestionsDroppableProps {
     localForm: TForm;
+    activeQuestionId: string | null;
+    setActiveQuestionId: (questionId: string | null) => void;
+    moveQuestion: (questionIndex: number, up: boolean) => void;
 }
 export const QuestionsDroppable = ({
-    localForm,  // form data
+    localForm,
+    activeQuestionId,
+    setActiveQuestionId,
+    moveQuestion,
  }: QuestionsDroppableProps) => {
     return (
         <div className="">
-            <QuestionCard 
-                localForm={localForm}
-            />
+            <SortableContext items={localForm.questions} strategy={verticalListSortingStrategy}>
+                {localForm.questions.map((question, questionIdx) => (
+                    <QuestionCard 
+                        localForm={localForm}
+                        question={question}
+                        activeQuestionId={activeQuestionId}
+                        setActiveQuestionId={setActiveQuestionId}
+                        questionIdx={questionIdx}
+                        moveQuestion={moveQuestion}
+                    />
+                ))}
+            </SortableContext>
         </div>
     )
 };
