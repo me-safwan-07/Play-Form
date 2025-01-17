42bf69f213c95cbe8544c770555780057ab9339e
me-safwan-07 <mesafwan07@gmail.com>
2025-01-17 22:50:54 +0530

    Enhance QuestionsDroppable component with additional question management functions and invalid question handling

--- client/src/pages/edit/components/QuestionsDroppable.tsx
+++ client/src/pages/edit/components/QuestionsDroppable.tsx
@@ -6,27 +6,46 @@ import { QuestionCard } from "./QuestionCard"
 
 interface QuestionsDroppableProps {
     localForm: TForm;
+    moveQuestion: (questionIndex: number, up: boolean) => void;
+    updateQuestion: (questionIndex: number, updatedAttributes: any) => void;
+    deleteQuestion: (questionIndex: number) => void
+    duplicateQuestion: (questionIndex: number) => void;
+    addQuestion: (questionIndex: number, index?: number) => void;
     activeQuestionId: string | null;
     setActiveQuestionId: (questionId: string | null) => void;
-    moveQuestion: (questionIndex: number, up: boolean) => void;
+    invalidQuestions: string[] | null;
+    internalQuestionIdMap: Record<string, string>;
 }
 export const QuestionsDroppable = ({
     localForm,
     activeQuestionId,
     setActiveQuestionId,
     moveQuestion,
+    updateQuestion,
+    invalidQuestions,
+    internalQuestionIdMap,
+    duplicateQuestion,
+    addQuestion,
+    deleteQuestion
  }: QuestionsDroppableProps) => {
     return (
         <div className="">
             <SortableContext items={localForm.questions} strategy={verticalListSortingStrategy}>
                 {localForm.questions.map((question, questionIdx) => (
-                    <QuestionCard 
+                    <QuestionCard
+                        key={internalQuestionIdMap[question.id]}
                         localForm={localForm}
                         question={question}
-                        activeQuestionId={activeQuestionId}
-                        setActiveQuestionId={setActiveQuestionId}
                         questionIdx={questionIdx}
                         moveQuestion={moveQuestion}
+                        updateQuestion={updateQuestion}
+                        duplicateQuestion={duplicateQuestion}
+                        deleteQuestion={deleteQuestion}
+                        activeQuestionId={activeQuestionId}
+                        setActiveQuestionId={setActiveQuestionId}
+                        lastQuestion={questionIdx === localForm.questions.length - 1}
+                        isInvalid={invalidQuestions ? invalidQuestions.includes(question.id) : false}
+                        addQuestion={addQuestion}
                     />
                 ))}
             </SortableContext>
