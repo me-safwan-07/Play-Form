cfea563359ca4093499b3c76e35e49a4cbd81a9c
me-safwan-07 <mesafwan07@gmail.com>
2025-01-17 22:50:38 +0530

    Enhance QuestionMenu component with question type management and update functionality

--- client/src/pages/edit/components/QuestionMenu.tsx
+++ client/src/pages/edit/components/QuestionMenu.tsx
@@ -1,4 +1,4 @@
-import { QUESTIONS_NAME_MAP } from "@/lib/questions";
+import { getQuestionDefaults, QUESTIONS_NAME_MAP } from "@/lib/questions";
 import { 
     DropdownMenu, 
     DropdownMenuItem, 
@@ -15,27 +15,93 @@ import {
     EllipsisIcon, 
     TrashIcon 
 } from "lucide-react";
-import { TFormQuestionTypeEnum } from "@/types/forms";
+import { TFormQuestion, TFormQuestionTypeEnum } from "@/types/forms";
+import { useState } from "react";
+import { createId } from "@paralleldrive/cuid2";
+import { ConfirmationModal } from "@/components/ui/ConfirmationModal";
 
 interface QuestionMenuProps {
     questionIdx: number;
+    lastQuestion: boolean;
+    duplicateQuestion: (questionIdx: number) => void;
+    deleteQuestion: (questionIdx: number) => void;
+    moveQuestion: (questionIdx: number, up: boolean) => void;
+    question: TFormQuestion;
+    // product: TProduct;
+    updateQuestion: (questionIdx: number, updatedAttributes: any) => void;
+    addQuestion: (question: any, index?: number) => void;
 }
 
 export const QuestionMenu = ({
     questionIdx,
+    lastQuestion,
+    duplicateQuestion,
+    deleteQuestion,
+    moveQuestion,
+    addQuestion,
+    question,
+    updateQuestion
 }: QuestionMenuProps) => {
 
-    // const addQuestionBelow = (index: TFormQuestionTypeEnum) => {
-    //     // const questionDefaults = getQuestionDefaults();
-    // }
+    const [changeToType, setChangeToType] = useState(question.type);
+
+    const changeQuationType = (type: TFormQuestionTypeEnum) => {
+        const { headline, required, subheader} = question;
+
+        // if going from single select to multi select or vice versa, we need to keep the choices as well
+
+        // if (
+        //     (type === TFormQuestionTypeEnum.MultipleChoiceSingle && 
+        //         question.type === TFormQuestionTypeEnum.MultipleChoiceMulti) ||
+        //     (type === TFormQuestionTypeEnum.MultipleChoiceMulti &&
+        //         question.type === TFormQuestionTypeEnum.MultipleChoiceSingle)
+        // )
+
+        updateQuestion(questionIdx, {
+            type,
+            headline,
+            subheader,
+            required,
+        });
+    }
+    
+
+    // const addQuestionBelow = (type: TFormQuestionTypeEnum) => {
+    //     const questionDefaults = getQuestionDefaults(type, ''); //add the product in empty string
+
+    //     addQuestion(
+    //         {
+    //             ...questionDefaults,
+    //             type,
+    //             id: createId,
+    //             required: true,
+    //         },
+    //         questionIdx + 1
+    //     );
+
+    //     //  scroll to the new question
+    //     const section = document.getElementById(`${question.id}`);
+    //     section?.scrollIntoView({ behavior: "smooth", block:"end", inline:"end" })
+    // };
+    
+    const onConfirm = () => {
+        changeQuationType(changeToType)
+    }
     return (
         <div className="flex space-x-2">
             <CopyIcon 
                 className="h-4 cursor-pointer text-slate-500 hover:text-slate-600"
-                // onClick button appears beacuse click this icon the question will make duplicate
+                onClick={(e) => {
+                    e.stopPropagation();
+                    duplicateQuestion(questionIdx);
+                }}
             />
             <TrashIcon 
                 className="h-4 cursor-pointer text-slate-500 hover:text-slate-600"
+                onClick={(e) => {
+                    e.stopPropagation();
+                    deleteQuestion(questionIdx);
+                }}
             />
 
             <DropdownMenu>
@@ -112,6 +178,8 @@ export const QuestionMenu = ({
                     </div>
                 </DropdownMenuContent>
             </DropdownMenu>
+
+            {/* confirmationModal not necessary because we dont have the logic in the question or API */}
         </div>
     )
 }
