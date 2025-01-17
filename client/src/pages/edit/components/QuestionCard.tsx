4964ca740a0370f498fd784a06df7f97c65bc27b
me-safwan-07 <mesafwan07@gmail.com>
2025-01-17 22:50:19 +0530

    Refactor QuestionCard component to enhance question management with additional props and implement text formatting for highlighted sections

--- client/src/pages/edit/components/QuestionCard.tsx
+++ client/src/pages/edit/components/QuestionCard.tsx
@@ -12,15 +12,21 @@ import { DateQuestionForm } from "./DateQuestionForm"
 import { AddressQuestionForm } from "./AddressQuestionForm"
 import { questionTypes } from "@/lib/questions"
 import { TForm, TFormEditorTabs, TFormQuestion, TFormQuestionTypeEnum } from "@/types/forms"
+import { isValid } from "date-fns";
 
 interface QuestionCardProps {
     localForm: TForm;
-    questionIdx: number;
     question: TFormQuestion;
+    questionIdx: number;
+    moveQuestion: (questionIndex: number, up: boolean) => void;
+    updateQuestion: (questionIndex: number, updatedAttributes: any) => void;
+    deleteQuestion: (questionIndex: number) => void
+    duplicateQuestion: (questionIndex: number) => void;
     activeQuestionId: string | null;
     setActiveQuestionId: (questionId: string | null) => void;
-    moveQuestion: (questionIndex: number, up: boolean) => void;
-
+    lastQuestion: boolean;
+    isInvalid: boolean;
+    addQuestion: (question: any, index?: number) => void;
 }
 export const QuestionCard = ({
     localForm,
@@ -28,13 +34,38 @@ export const QuestionCard = ({
     activeQuestionId,
     setActiveQuestionId,
     question,
-    moveQuestion
+    moveQuestion,
+    updateQuestion,
+    lastQuestion,
+    isInvalid,
+    deleteQuestion,
+    duplicateQuestion,
+    addQuestion,
 }: QuestionCardProps) => {
     const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({
         id: question.id,
     });
 
     const open = activeQuestionId === question.id;
+
+    // formats the texst to hightlight specific parts of the text with slashes
+    const formatTextWithSlashes = (text) => {
+        const regex = /\/(.*?)\\/g;
+        const parts = text.split(regex);
+
+        return parts.map((part, index) => {
+            // check if the part was inside slashes
+            if (index % 2 !== 0) {
+                return (
+                    <span key={index} className="mx-1 rounded-md bg-slate-100 p-1 text-xs">
+                        {part}
+                    </span>
+                );
+            } else {
+                return part;
+            }
+        });
+    };
 
     const [type, setType] = useState("MultipleChoiceQuestionForm");
 
@@ -51,17 +82,20 @@ export const QuestionCard = ({
             )}
             ref={setNodeRef}
             style={style}
+            id={question.id}
             >
             <div 
                 {...listeners}
                 {...attributes}
                 className={cn(
-                "bg-slate-700", // if open
+                open ? "bg-slate-700" : "bg-slate-400",
                 "top-0 w-[5%] rounded-l-lg p-2 text-center text-sm text-white hover:cursor-grab hover:bg-slate-600",
+                isInvalid && 'bg-red-400 hover:bg-red-600',
                 "flex flex-col items-center justify-between"
             )}>
                 {/* comimg functionlity here of quesion index number plus 1 */}
                 <span>{questionIdx + 1}</span>
+                
                 <button className="opacity-0 hover:cursor-move group-hover:opacity-100">
                     <GripIcon className="h-4 w-4"/>
                 </button>
@@ -88,7 +122,7 @@ export const QuestionCard = ({
                             </div>
                             <div className="grow" dir="auto">
                                 <p className="text-sm font-semibold">
-                                    {questionTypes[0].preset.headline.default}
+                                    {question.headline}
                                 </p>
                                 {!open && question?.required && (
                                     <p className="mt-1 truncate text-xs text-slate-500">{question?.required && "Required"}</p>
@@ -99,6 +133,13 @@ export const QuestionCard = ({
                         <div className="flex items-center space-x-2">
                             <QuestionMenu 
                                 questionIdx={questionIdx}
+                                lastQuestionIdx={lastQuestion}
+                                duplicateQuestion={duplicateQuestion}
+                                deleteQuestion={deleteQuestion}
+                                moveQuestion={moveQuestion}
+                                question={question}
+                                updateQuestion={updateQuestion}
+                                addQuestion={addQuestion}
                             />
                         </div>
                     </div>
@@ -107,9 +148,12 @@ export const QuestionCard = ({
                     {/* Question content */}
                     {question.type === TFormQuestionTypeEnum.OpenText ? (
                         <OpenQuestionForm 
-                            question={question}
                             localForm={localForm}
+                            question={question}
                             questionIdx={questionIdx}
+                            updateQuestion={updateQuestion}
+                            lastQuestion={lastQuestion}
+                            isInvalid={isInvalid}
                         />
                     ) : type === 'MultipleChoiceQuestionForm' ? (
                         <MultipleChoiceQuestionForm 
