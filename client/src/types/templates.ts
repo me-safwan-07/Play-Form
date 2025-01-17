1abc759a747812167a97892541430e4f7eedf9ea
me-safwan-07 <mesafwan07@gmail.com>
2025-01-17 22:48:08 +0530

    Add Zod validation schemas for template types in templates.ts

---
+++ client/src/types/templates.ts
@@ -0,0 +1,22 @@
+import z from 'zod';
+import { ZFormQuestion, ZFormThankYouCard, ZFormWelcomeCard } from './forms';
+
+export const ZTemplateRole = z.enum(["productManager", "customerSuccess", "marketing", "sales"]);
+export type TTemplateRole = z.infer<typeof ZTemplateRole>;
+
+export const ZTemplate = z.object({
+    name: z.string(),
+    description: z.string(),
+    icon: z.any().optional(),
+    role: ZTemplateRole.optional(),
+    industries: z.array(z.enum(["eCommerce", "saas", "other"])).optional(),
+    // objectives: z.array()
+    preset: z.object({
+        name: z.string(),
+        welcomeCard: ZFormWelcomeCard,
+        questions: ZFormQuestion,
+        thankYouCard: ZFormThankYouCard
+    }),
+});
+
+export type TTemplate = z.infer<typeof ZTemplate>;
