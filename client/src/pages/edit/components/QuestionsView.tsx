9fc5457ebcfaaa6356a08d5a9b34aa2e971314b8
me-safwan-07 <mesafwan07@gmail.com>
2025-01-17 22:51:20 +0530

    Enhance QuestionsView component with question management features including addition, deletion, duplication, and validation handling

--- client/src/pages/edit/components/QuestionsView.tsx
+++ client/src/pages/edit/components/QuestionsView.tsx
@@ -1,32 +1,131 @@
-import React, { SetStateAction } from "react";
+import {
+  DndContext,
+  DragEndEvent,
+  PointerSensor,
+  closestCorners,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import { createId } from "@paralleldrive/cuid2";
+import React, { SetStateAction, useEffect, useMemo } from "react";
+import toast from "react-hot-toast";
 import { AddQuestionButton } from "./AddQuestionButton";
 import { EditThankYouCard } from "./EditThankYouCard";
 import { EditWelcomeCard } from "./EditWelcomeCard";
 import { QuestionsDroppable } from "./QuestionsDroppable";
 import { TForm } from "@/types/forms";
+import { isCardValid } from "@/lib/validation";
 
 interface QuestionViewProps {
     localForm: TForm;
     setLocalForm: React.Dispatch<SetStateAction<TForm>>;
     activeQuestionId: string | null;
     setActiveQuestionId: (questionId: string | null) => void;
+    invalidQuestions: string[] | null;
+    setInvalidQuestions: React.Dispatch<SetStateAction<string[] | null>>;
 };
 
 export const QuestionsView = ({
-    activeQuestionId,
-    setActiveQuestionId,
     localForm,
     setLocalForm,
+    activeQuestionId,
+    setActiveQuestionId,
+    invalidQuestions,
+    setInvalidQuestions
 }: QuestionViewProps) => {
+    const internalQuestionIdMap = useMemo(() => {
+        return localForm.questions.reduce((acc: { [key: string]: string }, question) => {
+            acc[question.id] = createId();
+            return acc;
+        }, {} as { [key: string]: string });
+    }, [localForm.questions]);
 
-    // const updateQuestion = (questionIdx: number, updatedAttributes: any) => {
-    //     let updatedForm = { ...localForm };
-    //     if ("id" in updatedAttributes) {
-    //         // if the form whose id is to be changed is linked to logic of any other form then changing it
-    //         const initialQuestionId = updatedForm.questions[questionIdx].id;
-    //         updatedForm = handle
-    //     }
-    // }
+    const updateQuestion = (questionIdx: number, updatedAttributes: any) => {
+        let updatedForm = { ...localForm };
+        if ("id" in updatedAttributes) {
+            // if the form whose id is to be changed is linked to logic of any other form then changing it
+            const initialQuestionId = updatedForm.questions[questionIdx].id;
+            if(invalidQuestions?.includes(initialQuestionId)) {
+                setInvalidQuestions(
+                    invalidQuestions.map((id) => (id === initialQuestionId ? updatedAttributes.id : id))
+                );
+            };
+
+            // relink the question to internal Id
+            internalQuestionIdMap[updatedAttributes.id] =
+                internalQuestionIdMap[localForm.questions[questionIdx].id];
+            delete internalQuestionIdMap[localForm.questions[questionIdx].id];
+            setActiveQuestionId(updatedAttributes.id);
+        }
+        updatedForm.questions[questionIdx] = {
+            ...updatedForm.questions[questionIdx],
+            ...updatedAttributes 
+        };
+        setLocalForm(updatedForm);
+    };
+
+    const deleteQuestion = (questionIdx: number) => {
+        const questionId = localForm.questions[questionIdx].id;
+        const activeQuestionIdTemp = activeQuestionId ?? localForm.questions[0].id;
+        let updatedForm: TForm = { ...localForm };
+
+        // // check if we are recalling from this question
+        // updatedForm.questions.forEach((question) => {
+        //     if (question.headline.includes(`recall: ${question.headline}`)) {
+        //         const recallInfo = extractRecallInfo(question.headline)
+        //     }
+        // })
+
+        updatedForm.questions.splice(questionIdx, 1);
+        setLocalForm(updatedForm);
+        delete internalQuestionIdMap[questionId];
+        if (questionId === activeQuestionIdTemp) {
+            if (questionId === activeQuestionIdTemp) {
+                if (questionIdx <= localForm.questions.length && localForm.questions.length > 0) {
+                    setActiveQuestionId(localForm.questions[questionIdx % localForm.questions.length].id);
+                } else if (localForm.thankYouCard.enabled) {
+                    setActiveQuestionId("end");
+                }
+            }
+        }
+        toast.success("Question deleted.");
+    };
+
+    const duplicateQuestion = (questionIdx: number) => {
+        const questionToDuplicate = structuredClone(localForm.questions[questionIdx]);
+
+        const newQuestionId = createId();
+
+        // create a copy of the question with a new id
+        const duplicateQuestion = {
+            ...questionToDuplicate,
+            id: newQuestionId,
+        };
+
+        // inset the new question right after the original one
+        const updatedForm = { ...localForm };
+        updatedForm.questions.splice(questionIdx + 1, 0, duplicateQuestion);
+
+        setLocalForm(updatedForm);
+        setActiveQuestionId(newQuestionId);
+        internalQuestionIdMap[newQuestionId] = createId();
+
+        toast.success("Question duplicated.");
+    };
+
+    const addQuestion = (question: any, index?: number) => {
+        const updatedForm = { ...localForm };
+        
+        if (index) {
+            updatedForm.questions.splice(index, 0, { ...question, isDraft: true});
+        } else {
+            updatedForm.questions.push({...question, isDraft: true});
+        };
+
+        setLocalForm(updatedForm);
+        setActiveQuestionId(question.id);
+        internalQuestionIdMap[question.id] = createId();
+    };
 
     const moveQuestion = (questionIndex: number, up: boolean) => {
         const newQuestions = Array.from(localForm.questions);
@@ -35,7 +134,52 @@ export const QuestionsView = ({
         newQuestions.splice(destinationIndex, 0, reorderedQuestion);
         const updatedForm = { ...localForm, questions: newQuestions };
         setLocalForm(updatedForm);
+    };
+
+
+
+    useEffect(() => {
+        if(invalidQuestions === null) return;
+
+        const updateInvalidQuestions = (card, cardId, currentInvalidQuestions) => {
+            if (card.enabled && !isCardValid(card, cardId)) {
+                return currentInvalidQuestions.includes(cardId)
+                 ? currentInvalidQuestions
+                  : [...currentInvalidQuestions, cardId];
+            }
+            return currentInvalidQuestions.filter((id: string) => id !== cardId)
+        };
+
+        const updatedQuestionStart = updateInvalidQuestions(localForm.welcomeCard, "start", invalidQuestions);
+        const updatedQuestionEnd = updateInvalidQuestions(
+            localForm.thankYouCard, 
+            "end", 
+            updatedQuestionStart
+        );
+        
+        setInvalidQuestions(updatedQuestionEnd);
+    }, [localForm.welcomeCard, localForm.thankYouCard]);
+
+    const sensors = useSensors(
+        useSensor(PointerSensor, {
+            activationConstraint: {
+                distance: 5,
+            },
+        })
+    );
+
+    const onDragEnd = (event: DragEndEvent) => {
+        const { active, over } = event;
+
+        const newQuestions = Array.from(localForm.questions);
+        const sourceIndex = newQuestions.findIndex((question) => question.id === active.id);
+        const destinationIndex = newQuestions.findIndex((question) => question.id === over?.id);
+        const [reorderedQuestions] = newQuestions.slice(sourceIndex, 1);
+        newQuestions.splice(destinationIndex, 0, reorderedQuestions);
+        const updatedForm = { ...localForm, questions: newQuestions };
+        setLocalForm(updatedForm);
     }
+
     return (
         <div className="mt-16 w-full px-5 py-4">
             <div className="mb-5 flex w-full flex-col gap-5">
@@ -44,15 +188,24 @@ export const QuestionsView = ({
                     setLocalForm={setLocalForm}
                     setActiveQuestionId={setActiveQuestionId}
                     activeQuestionId={activeQuestionId}
+                    isInvalid={invalidQuestions ? invalidQuestions.includes("start") : false}                 
                 />
             </div>
             
-            <QuestionsDroppable 
-                localForm={localForm}
-                moveQuestion={moveQuestion}
-                activeQuestionId={activeQuestionId}
-                setActiveQuestionId={setActiveQuestionId}
-            />
+            <DndContext sensors={sensors} onDragEnd={onDragEnd} collisionDetection={closestCorners}>
+                <QuestionsDroppable 
+                    localForm={localForm}
+                    moveQuestion={moveQuestion}
+                    updateQuestion={updateQuestion}
+                    duplicateQuestion={duplicateQuestion}
+                    deleteQuestion={deleteQuestion}
+                    activeQuestionId={activeQuestionId}
+                    setActiveQuestionId={setActiveQuestionId}
+                    invalidQuestions={invalidQuestions}
+                    addQuestion={addQuestion}
+                    internalQuestionIdMap={internalQuestionIdMap}
+                />
+            </DndContext>
 
             <AddQuestionButton />
             <div className="mt-5 flex flex-col gap-5">
