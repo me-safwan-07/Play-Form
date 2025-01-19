0ddeaaa31ca21aaa875fc47b9395f3af49eef92c
me-safwan-07 <mesafwan07@gmail.com>
2025-01-19 23:25:35 +0530

    feat: create passport.ts

---
+++ server/src/utils/passport.ts
@@ -0,0 +1,29 @@
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import bcrypt from "bcryptjs";
+import { prisma } from "../database";
+
+passport.use(
+    new LocalStrategy(
+      { usernameField: "email" },
+      async (email: string, password: string, done) => {
+        try {
+          const user = await prisma.user.findUnique({ where: { email } });
+          if (!user) {
+            return done(null, false, { message: "User not found" });
+          }
+
+          const isPasswordValid = await bcrypt.compare(password, user.password);
+          if (!isPasswordValid) {
+            return done(null, false, { message: "Incorrect password" });
+          }
+
+          return done(null, user)
+        } catch (error) {
+            return done(error)
+        }
+    }),
+);
+
+
