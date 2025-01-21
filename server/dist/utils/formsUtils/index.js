aac0d91ddfb4c133b1d1365a6de701ea1e6c9c35
me-safwan-07 <mesafwan07@gmail.com>
2025-01-21 23:51:50 +0530

    feat: add buildWhereClause and buildOrderByClause functions to formsUtils for enhanced query capabilities

--- server/dist/utils/formsUtils/index.js
+++ server/dist/utils/formsUtils/index.js
@@ -1,6 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.transformPrismaSurvey = void 0;
+exports.buildOrderByClause = exports.buildWhereClause = exports.transformPrismaSurvey = void 0;
 const transformPrismaSurvey = (surveyPrisma) => {
     let segment = null;
     if (surveyPrisma.segment) {
@@ -10,3 +10,27 @@ const transformPrismaSurvey = (surveyPrisma) => {
     return transformedSurvey;
 };
 exports.transformPrismaSurvey = transformPrismaSurvey;
+const buildWhereClause = (filterCriteria) => {
+    var _a;
+    const whereClause = [];
+    // for name
+    if (filterCriteria === null || filterCriteria === void 0 ? void 0 : filterCriteria.name) {
+        whereClause.push({ name: { contains: filterCriteria.name, mode: "insensitive" } });
+    }
+    // for status
+    if ((filterCriteria === null || filterCriteria === void 0 ? void 0 : filterCriteria.status) && ((_a = filterCriteria === null || filterCriteria === void 0 ? void 0 : filterCriteria.status) === null || _a === void 0 ? void 0 : _a.length)) {
+        whereClause.push({ status: { in: filterCriteria.status } });
+    }
+    return { AND: whereClause };
+};
+exports.buildWhereClause = buildWhereClause;
+const buildOrderByClause = (sortBy) => {
+    if (!sortBy) {
+        return undefined;
+    }
+    if (sortBy === "name") {
+        return [{ name: 'asc' }];
+    }
+    return [{ [sortBy]: 'desc' }];
+};
+exports.buildOrderByClause = buildOrderByClause;
--- server/src/utils/formsUtils/index.ts
+++ server/src/utils/formsUtils/index.ts
@@ -1,21 +1,50 @@
-import { TForm } from "../../types/forms";
+import { Prisma } from "@prisma/client";
+import { TForm, TFormFilterCriteria } from "../../types/forms";
 import { TSegment } from "../../types/segment";
 
 export const transformPrismaSurvey = (surveyPrisma: any): TForm => {
-    let segment: TSegment | null = null;
-  
-    if (surveyPrisma.segment) {
-      segment = {
-        ...surveyPrisma.segment,
-        surveys: surveyPrisma.segment.surveys.map(({survey}: any) => survey.id),
-      };
-    }
-  
-    const transformedSurvey: TForm = {
-      ...surveyPrisma,
-      displayPercentage: Number(surveyPrisma.displayPercentage) || null,
-      segment,
+  let segment: TSegment | null = null;
+
+  if (surveyPrisma.segment) {
+    segment = {
+      ...surveyPrisma.segment,
+      surveys: surveyPrisma.segment.surveys.map(({survey}: any) => survey.id),
     };
-  
-    return transformedSurvey;
+  }
+
+  const transformedSurvey: TForm = {
+    ...surveyPrisma,
+    displayPercentage: Number(surveyPrisma.displayPercentage) || null,
+    segment,
   };
+
+  return transformedSurvey;
+};
+
+export const buildWhereClause = (filterCriteria: TFormFilterCriteria) => {
+  const whereClause: Prisma.FormWhereInput["AND"] = [];
+
+  // for name
+  if (filterCriteria?.name) {
+    whereClause.push({ name: { contains: filterCriteria.name, mode:"insensitive" } });
+  }
+
+  // for status
+  if (filterCriteria?.status && filterCriteria?.status?.length) {
+    whereClause.push({ status:  {in: filterCriteria.status }});
+  }
+
+  return { AND: whereClause };
+};
+
+export const buildOrderByClause = (sortBy: string): Prisma.FormOrderByWithRelationInput[] | undefined => {
+  if (!sortBy) {
+    return undefined;
+  }
+
+  if (sortBy === "name") {
+    return [{ name: 'asc' }];
+  }
+
+  return [{ [sortBy]: 'desc' }];
+}
