df16d6ba0097622e0bb51a880fb429c756cf34ee
me-safwan-07 <mesafwan07@gmail.com>
2025-01-21 23:51:28 +0530

    feat: enhance form types with new styling and verification schemas, and add filter criteria

--- server/dist/types/forms.js
+++ server/dist/types/forms.js
@@ -3,16 +3,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.ZFormInput = exports.ZFormQuestions = exports.ZFormQuestionType = exports.ZFormQuestion = exports.ZFormFileUploadQuestion = exports.ZFormOpenTextQuestion = exports.ZFormOpenTextQuestionInputType = exports.ZFormQuestionBase = exports.TFormQuestionTypeEnum = exports.ZFormUpdateInput = exports.ZFormStatus = exports.ZFormWelcomeCard = exports.ZFormThankYouCard = void 0;
+exports.ZFormFilters = exports.ZFormFilterCriteria = exports.ZFormUpdateInput = exports.ZFormInput = exports.ZFormQuestions = exports.ZFormQuestionType = exports.ZFormQuestion = exports.ZFormFileUploadQuestion = exports.ZFormOpenTextQuestion = exports.ZFormOpenTextQuestionInputType = exports.ZFormQuestionBase = exports.ZFormStatus = exports.ZFormWelcomeCard = exports.TFormQuestionTypeEnum = exports.ZFormVerifyEmail = exports.ZFormStyling = exports.ZFormThankYouCard = void 0;
 const zod_1 = __importDefault(require("zod"));
-const common_1 = require("./common");
-// Form Schema
-const ZForms = zod_1.default.object({
-    id: zod_1.default.string().cuid2(),
-    createdAt: zod_1.default.date(),
-    updatedAt: zod_1.default.date(),
-    name: zod_1.default.string(),
-});
+const styling_1 = require("./styling");
 // Thank You Card Schema
 exports.ZFormThankYouCard = zod_1.default.object({
     enabled: zod_1.default.boolean(),
@@ -22,25 +15,15 @@ exports.ZFormThankYouCard = zod_1.default.object({
     buttonLink: zod_1.default.string().optional(),
     imageUrl: zod_1.default.string().optional(),
 });
-// Welcome Card Schema
-exports.ZFormWelcomeCard = zod_1.default
-    .object({
-    enabled: zod_1.default.boolean(),
-    headline: zod_1.default.string().optional(),
-    fileUrl: zod_1.default.string().optional(),
-    buttonLabel: zod_1.default.string().optional(),
-    showResponseCount: zod_1.default.boolean().default(false),
-})
-    .refine((schema) => !(schema.enabled && !schema.headline), {
-    message: 'Welcome card must have a headline',
+exports.ZFormStyling = styling_1.ZBaseStyling.extend({
+    overwriteThemeStyling: zod_1.default.boolean().nullish(),
 });
-// Form Status Enum
-exports.ZFormStatus = zod_1.default.enum(['draft', 'scheduled', 'inProgress', 'paused', 'completed']);
-// Form Update Input
-exports.ZFormUpdateInput = ZForms.omit({ createdAt: true, updatedAt: true }).and(zod_1.default.object({
-    createdAt: zod_1.default.coerce.date(),
-    updatedAt: zod_1.default.coerce.date(),
-}));
+exports.ZFormVerifyEmail = zod_1.default
+    .object({
+    name: zod_1.default.optional(zod_1.default.string()),
+    subheading: zod_1.default.optional(zod_1.default.string()),
+})
+    .optional();
 // Question Types Enum
 var TFormQuestionTypeEnum;
 (function (TFormQuestionTypeEnum) {
@@ -51,6 +34,21 @@ var TFormQuestionTypeEnum;
     TFormQuestionTypeEnum["Date"] = "date";
     TFormQuestionTypeEnum["Address"] = "address";
 })(TFormQuestionTypeEnum || (exports.TFormQuestionTypeEnum = TFormQuestionTypeEnum = {}));
+// Welcome Card Schema
+exports.ZFormWelcomeCard = zod_1.default
+    .object({
+    enabled: zod_1.default.boolean(),
+    headline: zod_1.default.string().optional(),
+    fileUrl: zod_1.default.string().optional(),
+    // html is pending
+    buttonLabel: zod_1.default.string().optional(),
+    showResponseCount: zod_1.default.boolean().default(false),
+})
+    .refine((schema) => !(schema.enabled && !schema.headline), {
+    message: 'Welcome card must have a headline',
+});
+// Form Status Enum
+exports.ZFormStatus = zod_1.default.enum(['draft', 'scheduled', 'inProgress', 'paused', 'completed']);
 // Base Question Schema
 exports.ZFormQuestionBase = zod_1.default.object({
     id: zod_1.default.string(),
@@ -59,17 +57,17 @@ exports.ZFormQuestionBase = zod_1.default.object({
     subheader: zod_1.default.string().optional(),
     imageUrl: zod_1.default.string().optional(),
     required: zod_1.default.boolean(),
-    buttonLabel: zod_1.default.string().optional(),
-    backButtonLabel: zod_1.default.string().optional(),
-    scale: zod_1.default.enum(['number', 'smiley', 'star']).optional(),
-    range: zod_1.default.union([zod_1.default.literal(5), zod_1.default.literal(3), zod_1.default.literal(4), zod_1.default.literal(7), zod_1.default.literal(10)]).optional(),
+    // buttonLabel: z.string().optional(),
+    // backButtonLabel: z.string().optional(),
+    // scale: z.enum(['number', 'smiley', 'star']).optional(),
+    // range: z.union([z.literal(5), z.literal(3), z.literal(4), z.literal(7), z.literal(10)]).optional(),
     isDraft: zod_1.default.boolean().optional(),
 });
 // Open Text Question Schema
 exports.ZFormOpenTextQuestionInputType = zod_1.default.enum(['text', 'email', 'url', 'number', 'phone']);
 exports.ZFormOpenTextQuestion = exports.ZFormQuestionBase.extend({
     type: zod_1.default.literal(TFormQuestionTypeEnum.OpenText),
-    Placeholder: zod_1.default.string().optional(),
+    placeholder: zod_1.default.string().optional(),
     longAnswer: zod_1.default.boolean().optional(),
     inputType: exports.ZFormOpenTextQuestionInputType.optional().default('text'),
 });
@@ -78,7 +76,7 @@ exports.ZFormFileUploadQuestion = exports.ZFormQuestionBase.extend({
     type: zod_1.default.literal(TFormQuestionTypeEnum.FileUpload),
     allowMultiPleFiles: zod_1.default.boolean(),
     maxSizeInMB: zod_1.default.number().optional(),
-    allowedFileExtensions: zod_1.default.array(common_1.ZAllowedFileExtension).optional(),
+    // allowedFileExtensions: z.array(ZAllowedFileExtension).optional(),
 });
 // Union of Question Types
 exports.ZFormQuestion = zod_1.default.union([exports.ZFormOpenTextQuestion, exports.ZFormFileUploadQuestion]);
@@ -95,4 +93,40 @@ exports.ZFormInput = zod_1.default.object({
     welcomeCard: exports.ZFormWelcomeCard,
     thankYouCard: exports.ZFormThankYouCard,
     questions: exports.ZFormQuestions,
+});
+const ZForms = zod_1.default.object({
+    id: zod_1.default.string().cuid2(),
+    createdAt: zod_1.default.date(),
+    updatedAt: zod_1.default.date(),
+    name: zod_1.default.string(),
+    environmentId: zod_1.default.string(),
+    status: exports.ZFormStatus,
+    autoClose: zod_1.default.number().nullable(),
+    redirectUrl: zod_1.default.string().url().nullable(),
+    displayLimit: zod_1.default.number().nullable(),
+    welcomeCard: exports.ZFormWelcomeCard,
+    questions: exports.ZFormQuestions,
+    thankYouCard: exports.ZFormThankYouCard,
+    delay: zod_1.default.number(),
+    autoComplete: zod_1.default.number().nullable(),
+    runOnDate: zod_1.default.date().nullable(),
+    closeOnDate: zod_1.default.date().nullable(),
+    styling: exports.ZFormStyling.nullable(),
+    verifyEmail: exports.ZFormVerifyEmail.optional(),
+    resultShareKey: zod_1.default.string().nullable(),
+    displayPercentage: zod_1.default.string().nullish(),
+});
+exports.ZFormUpdateInput = ZForms.omit({ createdAt: true, updatedAt: true }).and(zod_1.default.object({
+    createdAt: zod_1.default.coerce.date(),
+    updatedAt: zod_1.default.coerce.date(),
+}));
+exports.ZFormFilterCriteria = zod_1.default.object({
+    name: zod_1.default.string().optional(),
+    status: zod_1.default.array(exports.ZFormStatus).optional(),
+    sortBy: zod_1.default.enum(['createdAt', 'updatedAt', 'name']).optional(),
+});
+exports.ZFormFilters = zod_1.default.object({
+    name: zod_1.default.string(),
+    status: zod_1.default.array(exports.ZFormStatus),
+    sortBy: zod_1.default.enum(['createdAt', 'updatedAt', 'name']),
 });
--- server/src/types/forms.ts
+++ server/src/types/forms.ts
@@ -1,15 +1,8 @@
 import z from 'zod';
-import { ZAllowedFileExtension } from './common';
+import { ZBaseStyling } from './styling';
 
-// Form Schema
-const ZForms = z.object({
-  id: z.string().cuid2(),
-  createdAt: z.date(),
-  updatedAt: z.date(),
-  name: z.string(),
-});
-
-export type TForm = z.infer<typeof ZForms>;
+// Form Editor Tabs Enum
+export type TFormEditorTabs = "questions" | "settings" | "styling";
 
 // Thank You Card Schema
 export const ZFormThankYouCard = z.object({
@@ -21,47 +14,18 @@ export const ZFormThankYouCard = z.object({
   imageUrl: z.string().optional(),
 });
 
-export type TFormThankYouCard = z.infer<typeof ZFormThankYouCard>;
+export const ZFormStyling = ZBaseStyling.extend({
+  overwriteThemeStyling: z.boolean().nullish(),
+})
 
-// Welcome Card Schema
-export const ZFormWelcomeCard = z
+export const ZFormVerifyEmail = z
   .object({
-    enabled: z.boolean(),
-    headline: z.string().optional(),
-    fileUrl: z.string().optional(),
-    buttonLabel: z.string().optional(),
-    showResponseCount: z.boolean().default(false),
+    name: z.optional(z.string()),
+    subheading: z.optional(z.string()),
   })
-  .refine((schema) => !(schema.enabled && !schema.headline), {
-    message: 'Welcome card must have a headline',
-  });
+  .optional();
 
-export type TFormWelcomeCard = z.infer<typeof ZFormWelcomeCard>;
-
-// Form Status Enum
-export const ZFormStatus = z.enum(['draft', 'scheduled', 'inProgress', 'paused', 'completed']);
-
-// Form Response Interface
-export interface FormResponse {
-  id: string;
-  name: string;
-  createdBy: string;
-  status?: string;
-  createdAt: Date;
-  updatedAt: Date;
-  welcomeCard: object;
-  thankYouCard: object;
-}
-
-// Form Update Input
-export const ZFormUpdateInput = ZForms.omit({ createdAt: true, updatedAt: true }).and(
-  z.object({
-    createdAt: z.coerce.date(),
-    updatedAt: z.coerce.date(),
-  })
-);
-
-export type TFormUpdateInput = z.infer<typeof ZFormUpdateInput>;
+export type TFormVerifyEmail = z.infer<typeof ZFormVerifyEmail>;
 
 // Question Types Enum
 export enum TFormQuestionTypeEnum {
@@ -73,47 +37,88 @@ export enum TFormQuestionTypeEnum {
   Address = 'address',
 }
 
-// Base Question Schema
-export const ZFormQuestionBase = z.object({
-  id: z.string(),
-  type: z.string(),
-  headline: z.string(),
-  subheader: z.string().optional(),
-  imageUrl: z.string().optional(),
-  required: z.boolean(),
+// Welcome Card Schema
+export const ZFormWelcomeCard = z
+.object({
+  enabled: z.boolean(),
+  headline: z.string().optional(),
+  fileUrl: z.string().optional(),
+  // html is pending
   buttonLabel: z.string().optional(),
-  backButtonLabel: z.string().optional(),
-  scale: z.enum(['number', 'smiley', 'star']).optional(),
-  range: z.union([z.literal(5), z.literal(3), z.literal(4), z.literal(7), z.literal(10)]).optional(),
-  isDraft: z.boolean().optional(),
+  showResponseCount: z.boolean().default(false),
+})
+.refine((schema) => !(schema.enabled && !schema.headline), {
+  message: 'Welcome card must have a headline',
 });
 
-// Open Text Question Schema
-export const ZFormOpenTextQuestionInputType = z.enum(['text', 'email', 'url', 'number', 'phone']);
-export type TFormOpenTextQuestionInputType = z.infer<typeof ZFormOpenTextQuestionInputType>;
 
-export const ZFormOpenTextQuestion = ZFormQuestionBase.extend({
-  type: z.literal(TFormQuestionTypeEnum.OpenText),
-  Placeholder: z.string().optional(),
-  longAnswer: z.boolean().optional(),
-  inputType: ZFormOpenTextQuestionInputType.optional().default('text'),
-});
 
-export type TFormOpenTextQuestion = z.infer<typeof ZFormOpenTextQuestion>;
+export type TFormThankYouCard = z.infer<typeof ZFormThankYouCard>;
 
-// File Upload Question Schema
-export const ZFormFileUploadQuestion = ZFormQuestionBase.extend({
-  type: z.literal(TFormQuestionTypeEnum.FileUpload),
-  allowMultiPleFiles: z.boolean(),
-  maxSizeInMB: z.number().optional(),
-  allowedFileExtensions: z.array(ZAllowedFileExtension).optional(),
-});
 
-// Union of Question Types
-export const ZFormQuestion = z.union([ZFormOpenTextQuestion, ZFormFileUploadQuestion]);
+export type TFormWelcomeCard = z.infer<typeof ZFormWelcomeCard>;
 
-export type TFormQuestion = z.infer<typeof ZFormQuestion>;
-export type TFormQuestions = z.infer<typeof ZFormQuestions>;
+// Form Status Enum
+export const ZFormStatus = z.enum(['draft', 'scheduled', 'inProgress', 'paused', 'completed']);
+
+// Form Response Interface
+export interface FormResponse {
+  id: string;
+  name: string;
+    createdBy: string;
+    status?: string;
+    createdAt: Date;
+    updatedAt: Date;
+    welcomeCard: object;
+    thankYouCard: object;
+  }
+  
+  // Form Update Input
+  
+  export type TFormUpdateInput = z.infer<typeof ZFormUpdateInput>;
+  
+  
+  // Base Question Schema
+  export const ZFormQuestionBase = z.object({
+    id: z.string(),
+    type: z.string(),
+    headline: z.string(),
+    subheader: z.string().optional(),
+    imageUrl: z.string().optional(),
+    required: z.boolean(),
+    // buttonLabel: z.string().optional(),
+    // backButtonLabel: z.string().optional(),
+    // scale: z.enum(['number', 'smiley', 'star']).optional(),
+    // range: z.union([z.literal(5), z.literal(3), z.literal(4), z.literal(7), z.literal(10)]).optional(),
+    isDraft: z.boolean().optional(),
+  });
+  
+  // Open Text Question Schema
+  export const ZFormOpenTextQuestionInputType = z.enum(['text', 'email', 'url', 'number', 'phone']);
+  export type TFormOpenTextQuestionInputType = z.infer<typeof ZFormOpenTextQuestionInputType>;
+  
+  export const ZFormOpenTextQuestion = ZFormQuestionBase.extend({
+    type: z.literal(TFormQuestionTypeEnum.OpenText),
+    placeholder: z.string().optional(),
+    longAnswer: z.boolean().optional(),
+    inputType: ZFormOpenTextQuestionInputType.optional().default('text'),
+  });
+  
+  export type TFormOpenTextQuestion = z.infer<typeof ZFormOpenTextQuestion>;
+  
+  // File Upload Question Schema
+  export const ZFormFileUploadQuestion = ZFormQuestionBase.extend({
+    type: z.literal(TFormQuestionTypeEnum.FileUpload),
+    allowMultiPleFiles: z.boolean(),
+    maxSizeInMB: z.number().optional(),
+    // allowedFileExtensions: z.array(ZAllowedFileExtension).optional(),
+  });
+  
+  // Union of Question Types
+  export const ZFormQuestion = z.union([ZFormOpenTextQuestion, ZFormFileUploadQuestion]);
+  
+  export type TFormQuestion = z.infer<typeof ZFormQuestion>;
+  export type TFormQuestions = z.infer<typeof ZFormQuestions>;
 
 // Question Type Enum
 export const ZFormQuestionType = z.enum([
@@ -133,3 +138,50 @@ export const ZFormInput = z.object({
 });
 
 export type TFormInput = z.infer<typeof ZFormInput>;
+
+const ZForms = z.object({
+  id: z.string().cuid2(),
+  createdAt: z.date(),
+  updatedAt: z.date(),
+  name: z.string(),
+  environmentId: z.string(),
+  status: ZFormStatus,
+  autoClose: z.number().nullable(),
+  redirectUrl: z.string().url().nullable(),
+  displayLimit: z.number().nullable(),
+  welcomeCard: ZFormWelcomeCard,
+  questions: ZFormQuestions,
+  thankYouCard: ZFormThankYouCard,
+  delay: z.number(),
+  autoComplete: z.number().nullable(),
+  runOnDate: z.date().nullable(),
+  closeOnDate: z.date().nullable(),
+  styling: ZFormStyling.nullable(),
+  verifyEmail: ZFormVerifyEmail.optional(),
+  resultShareKey: z.string().nullable(),
+  displayPercentage: z.string().nullish(),
+});
+export type TForm = z.infer<typeof ZForms>;
+
+export const ZFormUpdateInput = ZForms.omit({ createdAt: true, updatedAt: true }).and(
+  z.object({
+    createdAt: z.coerce.date(),
+    updatedAt: z.coerce.date(),
+  })
+);
+
+export const ZFormFilterCriteria = z.object({
+  name: z.string().optional(),
+  status: z.array(ZFormStatus).optional(),
+  sortBy: z.enum(['createdAt', 'updatedAt', 'name']).optional(),
+});
+
+export type TFormFilterCriteria = z.infer<typeof ZFormFilterCriteria>;
+
+export const ZFormFilters = z.object({
+  name: z.string(),
+  status: z.array(ZFormStatus),
+  sortBy: z.enum(['createdAt', 'updatedAt', 'name']),
+});
+
+export type TFormFilters = z.infer<typeof ZFormFilters>;
