c37d3445833c52f400c504d7604ad22fed68bac2
me-safwan-07 <mesafwan07@gmail.com>
2025-01-21 23:50:12 +0530

    feat: implement routing for forms, templates, and user authentication

---
+++ client/src/routes/index.tsx
@@ -0,0 +1,29 @@
+import NotFound from '@/components/ui/NotFound';
+import Analysis from '@/pages/analysis';
+import UserTable from '@/pages/apitest/GetUserById';
+import Auth from '@/pages/auth';
+import { AddQuestionButton } from '@/pages/edit/components/AddQuestionButton';
+import Form from '@/pages/formPage/page';
+import { TemplateContainer } from '@/pages/templates/components/TemplateContainer';
+import { Edit } from 'lucide-react';
+import { Routes, Route } from 'react-router-dom';
+
+const AppRouter = () => {
+    return (
+    
+            <Routes>
+            <Route path="/forms/" element={<Form />} />
+            <Route path="/forms/templates" element={<TemplateContainer />} />
+            <Route path="/forms/:id/edit" element={<Edit />} />
+            <Route path="forms/:id/analysis/*" element={<Analysis />} />
+            <Route path="/add" element={<AddQuestionButton />} />
+            <Route path="/add" element={<AddQuestionButton />} />
+            <Route path="/auth/*" element={<Auth />} />
+            <Route path="/getuser" element={<UserTable />} />
+            <Route path="*" element={<NotFound />} />
+            </Routes>
+      
+    );
+}
+
+export default AppRouter;
