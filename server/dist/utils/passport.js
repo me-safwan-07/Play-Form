dc66b39d36716efc4f9507f8c7904f389598aa35
me-safwan-07 <mesafwan07@gmail.com>
2025-01-23 23:39:39 +0530

    refactor: comment out unused passport-related code for clarity

--- server/dist/utils/passport.js
+++ server/dist/utils/passport.js
@@ -1,34 +1,25 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const passport_1 = __importDefault(require("passport"));
-const passport_local_1 = require("passport-local");
-const bcryptjs_1 = __importDefault(require("bcryptjs"));
-const database_1 = require("../database");
-passport_1.default.use(new passport_local_1.Strategy({ usernameField: "email" }, (email, password, done) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const user = yield database_1.prisma.user.findUnique({ where: { email } });
-        if (!user) {
-            return done(null, false, { message: "User not found" });
-        }
-        const isPasswordValid = yield bcryptjs_1.default.compare(password, user.password);
-        if (!isPasswordValid) {
-            return done(null, false, { message: "Incorrect password" });
-        }
-        return done(null, user);
-    }
-    catch (error) {
-        return done(error);
-    }
-})));
+// import passport from "passport";
+// import { Strategy as LocalStrategy } from "passport-local";
+// import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+// import bcrypt from "bcryptjs";
+// import { prisma } from "../database";
+// passport.use(
+//     new LocalStrategy(
+//       { usernameField: "email" },
+//       async (email: string, password: string, done) => {
+//         try {
+//           const user = await prisma.user.findUnique({ where: { email } });
+//           if (!user) {
+//             return done(null, false, { message: "User not found" });
+//           }
+//           const isPasswordValid = await bcrypt.compare(password, user.password);
+//           if (!isPasswordValid) {
+//             return done(null, false, { message: "Incorrect password" });
+//           }
+//           return done(null, user)
+//         } catch (error) {
+//             return done(error)
+//         }
+//     }),
+// );
