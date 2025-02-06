130ea3b6f64b65bf4301e85474831c1f41510130
me-safwan-07 <mesafwan07@gmail.com>
2025-02-06 23:15:55 +0530

    refactor: update FormEditor and HomePage components
    - Replaced PreviewForm with FormPreview in FormEditor
    - Added scroll-hidden class and adjusted layout in FormEditor
    - Removed unused React import in HomePage
    - Simplified component imports and code structure

--- clients/src/pages/FormEditorPage/components/FormEditor.tsx
+++ clients/src/pages/FormEditorPage/components/FormEditor.tsx
@@ -1,11 +1,10 @@
-import { PreviewForm } from "@/components/ui/PreviewSurvey"
 import { FormMenuBar } from "./FormMenuBar"
-// import { QuestionsAudienceTabs } from "./QuestionsStylingSettingsTabs"
 import { QuestionsView } from "./QuestionsView"
 import { TForm, TFormEditorTabs } from "@/types/forms";
 import { useEffect, useRef, useState } from "react";
 import { structuredClone } from "@/lib/pollyfills/structuredClone";
 import { LoadingSkeleton } from "./LoadingSkeleton";
+import { FormPreview } from "@/pages/PreviewPage";
 
 interface FormEditorProps {
     form: TForm;
@@ -46,7 +45,7 @@ export const FormEditor =({
     }
     return (
         <>
-            <div className="flex h-full w-full flex-col">
+            <div className="scroll-hidden flex h-full w-full flex-col">
                 <FormMenuBar
                     localForm={localForm}
                     setLocalForm={setLocalForm}
@@ -57,7 +56,7 @@ export const FormEditor =({
                     // setInvalidQuestions={setInvalidQuestions}
                     responseCount={responseCount}
                 />
-                <div className="relative z-0 flex flex-1 overflow-hidden">
+                <div className="relative z-0 flex flex-1">
                     <main
                         className="relative z-0 w-1/2 flex-1 overflow-y-auto focus:outline-none"
                         ref={formEditorRef}
@@ -78,15 +77,12 @@ export const FormEditor =({
                                 setInvalidQuestions={setInvalidQuestions}
                             />
                         )}
-
                     </main>
+
                     <aside className="group hidden flex-1 flex-shrink-0 items-center justify-center overflow-hidden border-l border  border-slate-100 bg-slate-50 py-6 md:flex md:flex-col">
-                        <PreviewForm 
+                        <FormPreview 
                             form={localForm}
                             questionId={activeQuestionId}
-                            environmentId={environmentId}
-                            previewType= "fullwidth"
-                            // onFileUpload={async(file) => {file.name}}
                         />
                     </aside>
                 </div>
--- clients/src/pages/HomePage/index.tsx
+++ clients/src/pages/HomePage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import { getFirstEnvironmentByUserId } from '@/hooks/environmentHooks';
 import Button from '@/components/ui/Button';
