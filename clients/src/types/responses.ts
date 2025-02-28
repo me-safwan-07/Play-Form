cbe96477b1a76de72db17be10d2a270dcbe142c4
me-safwan-07 <mesafwan07@gmail.com>
2025-02-28 20:16:58 +0530

    feat: add TypeScript types for form handling, responses, and storage configurations

--- clients/src/types/responses.ts
+++ clients/src/types/responses.ts
@@ -1,4 +1,5 @@
-import { z } from "zod";
+import { finished } from "stream";
+import { boolean, z } from "zod";
 
 export const ZResponseDataValue = z.union([
     z.string(),
@@ -12,4 +13,22 @@ export type TResponseDataValue = z.infer<typeof ZResponseDataValue>;
 export const ZResponseData = z.record(ZResponseDataValue);
 
 export type TResponseData = z.infer<typeof ZResponseData>;
+
+export const ZResponseHiddenFieldValue = z.record(z.union([z.string(), z.number(), z.array(z.string())]));
+export type TResponseHiddenFieldValue = z.infer<typeof ZResponseHiddenFieldValue>;
+
+export const ZResponseUpdate = z.object({
+    finished: z.boolean(),
+    data: ZResponseData,
+    meta: z
+        .object({
+            url: z.string().optional(),
+            soruce: z.string().optional(),
+            action: z.string().optional(),
+        })
+        .optional(),
+    hiddenFields: ZResponseHiddenFieldValue.optional(), 
+});
+
+export type TResponseUpdate = z.infer<typeof ZResponseUpdate>;
 
