4cd93ecac20ed28d7c08ae9bf9244c871f094ad2
me-safwan-07 <mesafwan07@gmail.com>
2025-03-09 15:45:38 +0530

    feat: implement environment service with CRUD operations and caching

---
+++ server/src/services/organisations.services.ts
@@ -0,0 +1,15 @@
+// import { prisma } from "../database";
+// import { TOrganization } from "../types/organisations.types";
+
+// export const getOrganizationByUserId = (userId: string, page?: number): Promise<TOrganization[]> => 
+//     caches(
+//         async() => {
+//             try {
+//                 const organisations = await prisma.organization.findMany({
+//                     where: {
+                        
+//                     }
+//                 })
+//             }
+//         }
+//     )
