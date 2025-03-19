e5b6ebda732107bbc67a344943b87837e5c1b44e
me-safwan-07 <mesafwan07@gmail.com>
2025-03-19 23:23:07 +0530

    feat: enhance user routes with input validation for better data integrity

--- servers/src/routes/user.routes.js
+++ servers/src/routes/user.routes.js
@@ -1,13 +1,55 @@
 import express from 'express';
 import { createUserController, deleteUserByIdController, getUserByEmailController, getUserController, updateUserController } from '../controllers/user.controller.js';
+import { body, param } from 'express-validator';
 
 const router = express.Router();
 
 // Create a new User
-router.get('/users/:id', getUserController);
-router.get('/users/email/:email', getUserByEmailController);
-router.put('/users/:personId', updateUserController);
-router.post('/users', createUserController);
-router.delete('/users/:id', deleteUserByIdController);
+router.get(
+    '/users/:id', 
+    [
+        param('id').isString().withMessage('ID must be a string'),
+    ],
+    getUserController
+);
+
+router.get(
+    '/users/email/:email',
+    [
+        param('email').isEmail().withMessage('Email must be a valid email address'),
+    ],
+    getUserByEmailController
+);
+
+router.put(
+    '/users/:personId', 
+    [
+        param('personId').isString().withMessage('Person ID must be a string'),
+        body('name').optional().isString().withMessage('Name must be a string'),
+        body('email').optional().isEmail().withMessage('Must be a valid email'),
+        body('emailVerified').optional().isISO8601().toDate().withMessage('Must be a valid date'),
+        body('imageUrl').optional().isURL().withMessage('Must be a valid URL'),
+        body('notificationSettings').optional().isObject().withMessage('Notification settings must be an object')
+    ],
+    updateUserController
+);
+
+router.post(
+    '/users', 
+    [
+        body('name').isString().withMessage('Name must be a string').notEmpty().withMessage('Name is required'),
+        body('email').isEmail().withMessage('Must be a valid email'),
+        body('emailVerified').optional().isISO8601().toDate().withMessage('Must be a valid date'),
+        body('identityProvider').isIn(['email', 'google']).withMessage('Invalid identity provider'),
+        body('identityProviderAccountId').optional().isString().withMessage('Identity provider account ID must be a string')
+    ],
+    createUserController);
+router.delete(
+    '/users/:id', 
+    [
+        param('id').isString().withMessage('ID must be a string'),
+    ], 
+    deleteUserByIdController
+);
 
 export default router;
